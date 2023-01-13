9d8644e3df25946a8c3c98820225f915bc77fd27
zideliu <34944964+zideliu@users.noreply.github.com>
2023-01-13 18:37:54 +0800

    使用DivineEngine的配置

--- Filter/script/File/bilibiliAccount.js
+++
@@ -1,5 +0,0 @@
-let body = $response.body
-body=JSON.parse(body)
-body['data']['sections'].splice(2,1)
-body=JSON.stringify(body)
-$done({body})
--- Filter/script/File/wykaola.js
+++
@@ -1,3 +0,0 @@
-var obj = JSON.parse($response.body);
-obj.body = null;
-$done({body: JSON.stringify(obj)}); 
